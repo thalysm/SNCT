e6b87eba792b4b885b1c8b0d0414eda9a3ec3f3c
thalysm <thalyscorreia@hotmail.com>
2018-09-23 20:03:54 -0300

    Site
    Semana Nacional de Ciencia e Tecnologia

---
+++ visao/js/main.js
@@ -0,0 +1,101 @@
+
+(function ($) {
+    "use strict";
+
+    /*==================================================================
+    [ Validate after type ]*/
+    $('.validate-input .input100').each(function () {
+        $(this).on('blur', function () {
+            if (validate(this) == false) {
+                showValidate(this);
+            }
+            else {
+                $(this).parent().addClass('true-validate');
+            }
+        })
+    })
+
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit', function () {
+        var check = true;
+        var validaAux = true;
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
+                showValidate(input[i]);
+                check = false;
+            }
+            if (confirmarEmail(input[i]) == false) {
+                // Validando confirma email ao clicar no subimit 
+                $(input[i]).parent().removeClass('true-validate');
+                showValidate(input[i]);
+                check = false;
+                validaAux = false;
+                alert("Confirmar corretamente e-mail");
+            }
+
+        }
+        if (validaAux) {
+            alert("O cadastro será continuado na pagina do Google Forms");
+        }
+
+        return check;
+    });
+
+
+
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
+            $(this).parent().removeClass('true-validate');
+        });
+    });
+
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('id') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if ($(input).val().trim() == '') {
+                return false;
+            }
+        }
+    }
+
+    //Fazer o confirmar email
+    function confirmarEmail(input) {
+        if ($(input).attr('id') == 'email2') {
+            if ($("#email").val() != $("#email2").val())
+                return false;
+            else
+                return true;
+        }
+    }
+
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+
+        $(thisAlert).append('<span class="btn-hide-validate">&#xf136;</span>')
+        $('.btn-hide-validate').each(function () {
+            $(this).on('click', function () {
+                hideValidate(this);
+            });
+        });
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+        $(thisAlert).removeClass('alert-validate');
+        $(thisAlert).find('.btn-hide-validate').remove();
+    }
+
+
+})(jQuery);
